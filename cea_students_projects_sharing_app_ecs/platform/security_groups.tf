4615959ec67ebfa2ee342cf5b4f1b8c7d20a234a
Silas Teixeira <silas.cloudspace@gmail.com>
2025-02-10 10:13:01 +0000

    added cea studenst projects sharing app

---
+++ cea_students_projects_sharing_app_ecs/platform/security_groups.tf
@@ -0,0 +1,59 @@
+# ALB security group
+resource "aws_security_group" "alb_security_group" {
+  name        = "${var.project_name}-${var.environment}-alb-sg"
+  description = "Enable HTTP/HTTPS access on ports 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-alb-sg"
+  }
+}
+
+# ECS security group
+resource "aws_security_group" "ecs_security_group" {
+  name        = "${var.project_name}-${var.environment}-ecs-sg"
+  description = "Allow HTTP traffic from ALB to fargate"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description     = "Allow HTTP traffic from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-app-sg"
+  }
+}
